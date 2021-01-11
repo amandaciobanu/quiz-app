abfdf0d5f21b4350512370d16b60ad7495d3aae2
Amanda Ciobanu <amanda@ciobanu.org>
2021-01-11 18:13:29 +0000

    feat: setup inner html which will be fetched by api and question card to accept props

--- quiz-app/src/components/QuestionCard.tsx
+++ quiz-app/src/components/QuestionCard.tsx
@@ -3,14 +3,31 @@ import React from 'react';
 
 type Props = {
     question: string;
-    answer: string[];
+    answers: string[];
     callback: any;
-    userAnswer: string;
+    userAnswer: boolean;
     questionNumber: number;
     totalQuestion: number;
 };
-const QuestionCart: React.FC<Props> = () => (
-
-    <div>Question Card</div>);
+const QuestionCart: React.FC<Props> = ({
+                                           question,
+                                           answers,
+                                           callback,
+                                           userAnswer,
+                                           questionNumber,
+                                           totalQuestion}) => (
+    <div>
+        <p className='number'>Question: {questionNumber} / {totalQuestion}</p>
+        <p dangerouslySetInnerHTML={{__html: question}}/>
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{__html: answer}}/>
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>);
 
 export default QuestionCart;
