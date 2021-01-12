d6aae18f443a9fb71801809006cda66555371e07
Amanda Ciobanu <amanda@ciobanu.org>
2021-01-12 15:02:04 +0000

    style: create style for questions and if answer is correct or not. Applied css to QuestionCard

--- quiz-app/src/components/QuestionCard.tsx
+++ quiz-app/src/components/QuestionCard.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-//types
-
+//Types
 import {AnswerObject} from "../App";
 
 //Styles
-
+import {Wrapper, ButtonWrapper} from "./QuestionCards.styles";
 
 type Props = {
     question: string;
@@ -22,18 +21,21 @@ const QuestionCard: React.FC<Props> = ({
                                            userAnswer,
                                            questionNumber,
                                            totalQuestion}) => (
-    <div>
+    <Wrapper>
         <p className='number'>Question: {questionNumber} / {totalQuestion}</p>
         <p dangerouslySetInnerHTML={{__html: question}}/>
         <div>
             {answers.map((answer) => (
-                <div key={answer}>
+                <ButtonWrapper
+                    key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}>
                     <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{__html: answer}}/>
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div>);
+    </Wrapper>);
 
 export default QuestionCard;
